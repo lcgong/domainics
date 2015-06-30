1196337db9048e44e257810872178d2784eb162a
Chenggong Lyu <lcgong@gmail.com>
2015-06-30 08:13:25 +0800

    refactor the dobject

--- test/test_domobj.py
+++ test/test_domobj.py
@@ -1,68 +1,214 @@
 
 import unittest
 
-from tornice.domobj import dobject, dfield, dlist
+from tornice.domobj import dobject, dfield, identity, agg
+
+
 
 class TestDomainObject(unittest.TestCase):
 
-    def test_dobject_eq(self):
+    @unittest.skip('')
+    def test_new(self):
         class A(dobject):
-            no = dfield(int, expr='1001')
+            a = dfield(int, expr="100")
 
-        class B(dobject):
-            no = dfield(int, expr='1001')
+        class B(A):
+            b = dfield(int, expr='201')
+            m = dfield(int, expr='202')
 
-        a1, a2, b = A(), A(), B()
+        class C(B):
+            c = dfield(int, expr='300')
+
+        c = C(310, 210, a=150)
+        self.assertEqual(c.c, 310)
+        self.assertEqual(c.b, 210)
+        self.assertEqual(c.m, 202)
+        self.assertEqual(c.a, 150)
+
+        with self.assertRaises(ValueError) as ex:
+            c = C(310, x=150) # the attribute x is not defined
+
+
+    @unittest.skip('')
+    def test_identity(self):
+
+        class A(dobject):
+            a_no = dfield(str, expr="'100'")
+            amt  = dfield(float)
+
+            identity(a_no)
+
+        a = A(a_no=200)
+        self.assertEqual(a._dobj_id_names, ['a_no'])
+        self.assertEqual(a._dobj_id.a_no, 200)
+        self.assertEqual(a._dobj_id, (200,))
+
+        class A(dobject):
+            a_no = dfield(str, expr="'100'")
+            b_no = dfield(str, expr="'101'")
+            amt  = dfield(float)
+
+            identity(a_no, b_no)
+
+        a = A(a_no=200, b_no=300)
+        self.assertEqual(a._dobj_id_names, ['a_no', 'b_no'])
+        self.assertEqual(a._dobj_id.a_no, 200)
+        self.assertEqual(a._dobj_id, (200,300))
+
+
+        class A(dobject):
+            a_no = dfield(str, expr="'100'")
+            b_no = dfield(str, expr="'101'")
+            amt  = dfield(float)
+
+        a = A()
+        self.assertEqual(a._dobj_id_names, [])
+        self.assertIsNone(a._dobj_id)        
+
+
+        class A(dobject):
+            no1 = dfield(int, expr="100")
+            no2 = dfield(int, expr="101")
+            amt = dfield(float)
+            age = dfield(int)
+
+            identity(no1, no2)
+
+        a1 = A(no1=100, no2=200, amt=3333, age=30)
+        a2 = A(no1=100, no2=200, amt=2222, age=30)
+        a3 = A(no1=100, no2=201, amt=2222, age=30)
         self.assertEqual(a1, a2)
-        self.assertEqual(a1, b) # ducking equivalent
-
-        class B(dobject):
-            no = dfield(int, expr='1001')
-            sn = dfield(int, expr='1000')
-
-        a, b = A(), B()
-        self.assertNotEqual(a, b)
-
-        b1, b2 = B(), B()
-        b1.no = 888
-        self.assertEqual(len(b1.__orig__), 1)
-        self.assertEqual(b1.__orig__['no'], 1001)
-
-        b1.no = 999 # the original value does not been chaned.
-        self.assertEqual(len(b1.__orig__), 1)
-        self.assertEqual(b1.__orig__['no'], 1001)
-
-        self.assertNotEqual(b1, b2)     
-        b1.no = 1001
-        self.assertEqual(len(b1.__orig__), 0)
-        self.assertEqual(b1, b2)        
-
-    def test_dfield_is_dobject(self):
+        self.assertNotEqual(a2, a3)
 
         class A(dobject):
-            no = dfield(int, expr='1001')
+            no1 = dfield(int, expr="100")
+            no2 = dfield(int, expr="101")
+            amt = dfield(float)
+            age = dfield(int)
 
-        class B(dobject):
-            sn = dfield(int)
-            a  = dfield(A, expr="A(no=123)", doc='A domain object')
+        a1 = A(no1=100, no2=200, amt=3333, age=30)
+        a2 = A(no1=100, no2=200, amt=2222, age=30)
+        a3 = A(no1=100, no2=201, amt=2222, age=30)
+        a4 = A(no1=100, no2=201, amt=2222, age=30)
 
-        b1, b2 = B(sn=1001, a=A(no=2001)), B(sn=1001, a=A(no=2001))
-        self.assertEqual(b1, b2)                
+        self.assertNotEqual(a1, a2)
+        self.assertNotEqual(a2, a3)
+        self.assertEqual(a3, a4)
 
-        b1, b2 = B(sn=1001, a=A(no=2001)), B(sn=1001, a=A(no=2002))
-        self.assertNotEqual(b1, b2)             
-        b2.a.no = 2001
-        self.assertEqual(b1, b2)
+    @unittest.skip('')
+    def test_identity_setattr(self):
 
-
-    def test_dlist(self):
         class A(dobject):
-            lst = dlist(int,  doc='a string list')
+            no1 = dfield(int, expr="'100'")
+            amt = dfield(float)
 
-        a = A(no=101)
-        print(a)
-        self.assertEqual(a.no, 101)
-        self.assertEqual(a.lst, [])
+            identity(no1)
+
+        with self.assertRaises(TypeError):
+            a = A()
+            a.no1 = 123 # the identity attribute is read-only.
+
+
+        class A(dobject):
+            no1 = dfield(str, expr="'100'")
+
+        a = A(100) # implict casting
+        self.assertEqual(a.no1, '100') 
+
+        a = A(no1=100)
+        self.assertEqual(a.no1, '100')
+
+        a = A('100')
+        a.no1 = 200 # the identity attr is read-only.
+        self.assertEqual(a.no1, '200')
+
+
+        class A(dobject):
+            no1 = dfield(float)
+
+        with self.assertRaises(TypeError):
+            a = A('dddd')
+
+
+    # @unittest.skip('')
+    def test_agg(self):
+        class Item(dobject):
+            item_no = dfield(str)
+            amt     = dfield(float)
+            identity(item_no)
+
+        class Bill(dobject):
+            doc_no = dfield(str)
+            items  = agg(Item)
+
+            identity(doc_no)
+
+        bill = Bill(doc_no='123')
+        print(bill)
+        bill.items.append(Item('no001', 100.0))
+
+        print(bill.items)
+        print(bill.items.identified)
+        print(bill.items.unidentified)
+
+    # def test_dobject_eq(self):
+    #     class A(dobject):
+    #         no = dfield(int, expr='1001')
+
+    #     class B(dobject):
+    #         no = dfield(int, expr='1001')
+
+    #     a1, a2, b = A(), A(), B()
+    #     self.assertEqual(a1, a2)
+    #     self.assertEqual(a1, b) # ducking equivalent
+
+    #     class B(dobject):
+    #         no = dfield(int, expr='1001')
+    #         sn = dfield(int, expr='1000')
+
+    #     a, b = A(), B()
+    #     self.assertNotEqual(a, b)
+
+    #     b1, b2 = B(), B()
+    #     b1.no = 888
+    #     self.assertEqual(len(b1.__orig__), 1)
+    #     self.assertEqual(b1.__orig__['no'], 1001)
+
+    #     b1.no = 999 # the original value does not been chaned.
+    #     self.assertEqual(len(b1.__orig__), 1)
+    #     self.assertEqual(b1.__orig__['no'], 1001)
+
+    #     self.assertNotEqual(b1, b2)     
+    #     b1.no = 1001
+    #     self.assertEqual(len(b1.__orig__), 0)
+    #     self.assertEqual(b1, b2)        
+
+    # def test_dfield_is_dobject(self):
+
+    #     class A(dobject):
+    #         no = dfield(int, expr='1001')
+
+    #     class B(dobject):
+    #         sn = dfield(int)
+    #         a  = dfield(A, expr="A(no=123)", doc='A domain object')
+
+    #     b1, b2 = B(sn=1001, a=A(no=2001)), B(sn=1001, a=A(no=2001))
+    #     self.assertEqual(b1, b2)                
+
+    #     b1, b2 = B(sn=1001, a=A(no=2001)), B(sn=1001, a=A(no=2002))
+    #     self.assertNotEqual(b1, b2)             
+    #     b2.a.no = 2001
+    #     self.assertEqual(b1, b2)
+
+
+    # def test_dlist(self):
+    #     class A(dobject):
+    #         lst = dlist(int,  doc='a string list')
+
+    #     a = A(no=101)
+    #     print(a)
+    #     self.assertEqual(a.no, 101)
+    #     self.assertEqual(a.lst, [])
         # self.assertEqual(a.lst, [10, 20, 30])
 
 
--- tornice/domobj.py
+++ tornice/domobj.py
@@ -1,22 +1,81 @@
 # -*- coding: utf-8 -*-
 
 
-import logging
+"""
+Domain: 
+A sphere of knowledge (ontology), influence, or activity. The subject area 
+to which the user applies a program is the domain of the software.
 
+Entity: 
+An object that is not defined by its attributes, but rather by a thread of 
+continuity and its identity.
+
+Value Object: 
+An object that contains attributes but has no conceptual identity. 
+They should be treated as immutable.
+
+Aggregate: 
+A collection of objects that are bound together by a root entity, 
+otherwise known as an aggregate root. 
+The aggregate root guarantees the consistency of changes being made within the aggregate 
+by forbidding external objects from holding references to its members.
+
+"""
+
+import logging
 _logger = logging.getLogger(__name__)
 
+import sys
+import inspect
+from collections import OrderedDict
+from collections import namedtuple
 
-from abc import abstractmethod
-from collections import OrderedDict as _Dict
+
+def identity(*fields):
+    """ """
+
+    # the primary_key is called class block,
+    # pass fields to metaclass '__new__'  via _dobj_pks_pendings
+    frame = sys._getframe(1)
+    if not hasattr(identity, '_dobj_pks_pendings'):
+        pendings = {}
+        setattr(identity, '_dobj_pks_pendings', pendings)
+    else:
+        pendings = getattr(identity, '_dobj_pks_pendings')
+
+    pks = []
+    for field in fields:
+        if not isinstance(field, dfield):
+            errmsg = 'the primary key \'%s\' is not dfield' % field
+            raise TypeError(errmsg)
+        
+        pks.append(field)
+        field.is_identity = True
+
+    f_locals = frame.f_locals
+    pendings[f_locals['__module__'] + '.' + f_locals['__qualname__']] = pks
+
 
 class DObjectMetaClass(type):
     @classmethod
-    def __prepare__(metacls, name, bases): 
-        return _Dict()
+    def __prepare__(metacls, name, bases, **kwargs): 
+        return OrderedDict()
 
     def __new__(metacls, classname, bases, class_dict):
 
-        fields = _Dict()
+
+        if hasattr(identity, '_dobj_pks_pendings'):
+            pk_pendings = getattr(identity, '_dobj_pks_pendings')
+            qclsname = class_dict.get('__module__') + '.' + class_dict.get('__qualname__')
+            if qclsname in pk_pendings:
+                pkeys = pk_pendings.pop(qclsname)
+            else:
+                pkeys = []
+        else:
+            pkeys = []
+
+
+        fields = OrderedDict()
         # set all names of domain field 
         for name, attr in class_dict.items():
             if isinstance(attr, dfield):
@@ -24,36 +83,66 @@ class DObjectMetaClass(type):
                 fields[name] = attr
 
         cls = type.__new__(metacls, classname, bases, class_dict)
-        cls.__fields__ = fields
+        cls._dobj_fields = fields
+        
+        id_fields = [p.name for p in pkeys] 
+        cls._dobj_id_names = id_fields
+
+        # assemble the _dobj_id property
+        if id_fields:
+            id_type   = namedtuple('ID', id_fields)
+
+        def _get_dobj_id(self):
+            """return the identity object of domain object; None if no identity is defined"""
+
+            if self.__class__._dobj_id_names:
+                values = []
+                for attrname in id_fields:
+                    val = getattr(self, attrname)
+                    if val is None:
+                        errmsg = "The id-attribute '%s' cannot be none" % attrname
+                        raise TypeError(errmsg) 
+                    values.append(val)
+                return id_type(*values)
+            
+            return None
+
+        cls._dobj_id = property(_get_dobj_id)
+
+
         return cls
 
 class dfield:
-    __slots__ = ('name', 'datatype', 'default_expr', '__doc__')
+    __slots__ = ('name', 'datatype', 'default_expr', '__doc__', 'is_identity')
 
     def __init__(self, type=object, expr=None, doc=None):
         self.datatype     = type
         self.default_expr = expr
+        self.is_identity  = False
         self.__doc__      = doc
+
 
     def __get__(self, instance, owner):
         if instance is None: # get domain field
             return self
 
-        return instance.__fields__.get(self.name)
+        return getattr(instance, '_dobject__attrs').get(self.name)
 
     def __set__(self, instance, value):
         name = self.name
-        if not isinstance(value, self.datatype):
-            err = 'field %s should be \'%s\' type: value=%r'
-            err %= (name, self.datatype.__name__, value)
-            raise TypeError(err)
+        if self.is_identity:
+            errmsg = "The identity attribute '%s' is read-only" % self.name
+            raise TypeError(errmsg)
 
-        fields  = instance.__fields__
-        now_val = fields.get(name, None)
+        value = cast_attr_value(name, value, self.datatype)
+  
+
+        attrs  = getattr(instance, '_dobject__attrs')
+        now_val = attrs.get(name, None)
         if now_val != value :
-            fields[name] = value
+            attrs[name] = value
 
-            changed = instance.__orig__
+            changed = getattr(instance, '_dobject__orig')
             old_val = changed.get(name, None)
             if old_val == value : # the value is recoveried
                 del changed[name]
@@ -63,13 +152,6 @@ class dfield:
 
     def __delete__(self, instance):
         raise NotImplemented('unsupported field deleting')
-
-class dlist(dfield):
-    
-    def __init__(self, type=object, doc=None):
-        self.datatype     = type
-        self.default_expr = None
-        self.__doc__      = doc
 
 class DomainObject(metaclass=DObjectMetaClass):
     
@@ -81,66 +163,65 @@ class DomainObject(metaclass=DObjectMetaClass):
         raise NotImplementedError()
     
 
-class DObjectList(DomainObject, list):
-    __orig_list__ = []
+def cast_attr_value(attrname, val, datatype):
+    if val is None:
+        return val
 
-    def __init__(self, iterable=None):
+    if isinstance(val, datatype):
+        return val
 
-        self.__orig_list__ = list(iterable)
-
-        super(DObjectList, self).__init__(iterable)
-
-    @property
-    def _pristine(self):
-        "True if this object are not changed"
-
-        if self != self.__orig_list__:
-            return False
-
-        for value in self:
-            if isinstance(value, DomainObject):
-                if not value._pristine:
-                    return False
-
-        return True
-
-    def _set_pristine(self):
-        self.__orig_list__.clear()
-        self.__orig_list__.extend(self)
-
-        for value in self:
-            if isinstance(value, DomainObject):
-                value._set_pristine()
-
+    try:
+        return datatype(val)
+    except ValueError as ex:
+        err = "The attribute '%s' should be \'%s\' type: value=%r(%s)"
+        err %= (attrname, datatype, val, type(val).__name__)
+        raise TypeError(err)            
 
 class dobject(DomainObject):
 
-    def __new__(cls, **kwargs):
-        """ """
+    def __new__(cls, *values, **kwvalues):
+        """ 
+        ordinal value: in defined order and mro
+        """
 
-        classes = []
+        seen = set()
+        fields = []
         for c in cls.__mro__:
-            if c == dobject: break
-            classes.append(c)
+            if c == dobject: 
+                break
 
-        fields = _Dict()
-        for c in reversed(classes):
-            for name in c.__fields__:
-                val = kwargs.pop(name, None)
-                if val is None: # default value
-                    expr = c.__fields__[name].default_expr
-                    val = eval(expr) if expr is not None else None
-                
-                fields[name] = val
+            for n, f in c._dobj_fields.items():
+                seen.add(n)
+                fields.append((n, f))
 
-        if kwargs:
+        attr_values = OrderedDict()
+        for val in values:
+            name, field = fields.pop(0)
+            attr_values[name] = cast_attr_value(name, val, field.datatype)
+
+
+        for name, field in fields:
+            val = kwvalues.pop(name, None)
+            val = cast_attr_value(name, val, field.datatype)
+            if val is None: # default value
+                expr = field.default_expr
+                if expr is not None:
+                    val = eval(expr)
+                else:
+                    val = None
+
+            attr_values[name] = val
+
+        if kwvalues:
             errmsg = 'not defined field: ' 
-            errmsg += ', '.join([f for f in kwargs])
+            errmsg += ', '.join([f for f in kwvalues])
             raise ValueError(errmsg)
 
         instance = super(dobject, cls).__new__(cls)
-        instance.__fields__  = fields
-        instance.__orig__ = {}
+        instance.__attrs = attr_values
+        instance.__orig  = {} # original atribute values
+
+        instance._dobj_id
 
         return instance
 
@@ -149,20 +230,31 @@ class dobject(DomainObject):
         errmsg %= (self.__class__.__name__, name)
         raise AttributeError(errmsg)
 
+
+
     def __repr__(self):
         """ """
-        segs = ['%s=%r' % p for p in self.__fields__.items()]
-        if self.__orig__:
-            args = ', '.join(['%s=%r' % p for p in self.__orig__.items() ])
-            segs.append('__orig__=(' + args + ')')
+        segs = ['%s=%r' % p for p in self.__attrs.items()]
+        if self.__orig:
+            args = ', '.join(['%s=%r' % p for p in self.__orig.items() ])
+            segs.append('__orig=(' + args + ')')
 
         return self.__class__.__name__ + '(' + ', '.join(segs) + ')'
 
     def __eq__(self, other) :
-        ''' ducking equals: True if all of fields of this object are equaled those of other '''
+        ''' ducking equals: 
+        1. True if the identity of this object equals that of other;
+        2. True if all of fields of this object equal those of other '''
 
-        if set(self.__fields__.keys()) != other.__fields__.keys():
+        this_id = self._dobj_id
+        if this_id is not None and this_id == other._dobj_id:
+            return True
+
+
+        if set(self.__fields__.keys()) != set(other.__fields__.keys()):
             return False
+
+
 
         for name, val in self.__fields__.items():
             if val != other.__fields__[name]:
@@ -173,7 +265,7 @@ class dobject(DomainObject):
     def _pristine(self):
         "True if this object are not changed"
         
-        if self.__orig__:
+        if self.__dobj_orig:
             return False
 
         for field in self.__class__.__mro_fields__:
@@ -182,6 +274,18 @@ class dobject(DomainObject):
                     return False
 
         return True
+
+    @classmethod
+    def primary_keys(cls, *fields):
+        for field in fields:
+            if not isinstance(field, dfield):
+                errmsg = 'the primary key \'%s\' is not dfield' % field
+                raise TypeError(errmsg)
+
+            cls._primary_keys.add(field)
+
+    def get_primary_keys(cls):
+        return
 
     @classmethod
     def __get_field__(cls, name):
@@ -215,3 +319,109 @@ class dobject(DomainObject):
 
                 seen.add(field)
                 yield field
+
+
+
+class agg(DomainObject):
+    """
+    Aggregate
+    """
+
+    def __init__(self, item_type, iterable=None):
+        self.identified   = OrderedDict()
+        self.unidentified = []
+        self.item_type = item_type
+
+        if iterable is not None:
+            for obj in iterable:
+                self._dobj_append(obj)
+
+    
+    def _dobj_append(self, obj):
+        obj_id = obj._dobj_id
+        if obj_id:
+            self.identified[obj_id] = obj
+        else:
+            self.unidentified.append(obj)
+
+    def append(self, obj):
+        self._dobj_append(obj)
+
+
+    # @property
+    # def _pristine(self):
+    #     "True if this object are not changed"
+
+    #     if self != self.__orig_list__:
+    #         return False
+
+    #     for value in self:
+    #         if isinstance(value, DomainObject):
+    #             if not value._pristine:
+    #                 return False
+
+    #     return True
+
+    # def _set_pristine(self):
+    #     self.__orig_list__.clear()
+    #     self.__orig_list__.extend(self)
+
+    #     for value in self:
+    #         if isinstance(value, DomainObject):
+    #             value._set_pristine()
+
+    def _dobj_fire_changed(self):
+        print('changed')
+
+    def __delitem__(self, i):
+        self._dobj_fire_changed()
+        return  super(dlist, self).__delitem__(i)
+
+    def __iadd__(self, l):
+        self._dobj_fire_changed()
+        return  super(dlist, self).__iadd__(l)
+
+    def __imul__(self, n):
+        self._dobj_fire_changed()
+        return  super(dlist, self).__imul__(n)   
+
+    def __setitem__(self, i, x):
+        self._dobj_fire_changed()
+        return  super(dlist, self).__setitem__(i, x)
+
+
+    def insert(self, idx, obj):
+        self._dobj_fire_changed()
+        return  super(dlist, self).insert(idx, obj)
+
+    def clear(self):
+        self._dobj_fire_changed()
+        return  super(dlist, self).clear()
+
+    def extend(self, iterable):
+        self._dobj_fire_changed()
+        return  super(dlist, self).extend(iterable)
+
+    def pop(self, *args):
+        self._dobj_fire_changed()
+        return  super(dlist, self).pop(*args)
+
+    def remove(self, value):
+        self._dobj_fire_changed()
+        return  super(dlist, self).remove(value)
+
+    def reverse(self):
+        self._dobj_fire_changed()
+        return  super(dlist, self).reverse()
+
+    def sort(self, key=None, reverse=False):
+        self._dobj_fire_changed()
+        return  super(dlist, self).reverse(key, reverse)
+
+class dcollection(dfield):
+    # grouped by pks, and sort in 
+    pass
+
+# class DCollectionObject(object):
+#     _dobj_
+
