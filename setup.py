6e4de85499c0ec91a09fefd0df60fddb6f991030
Chenggong Lyu <lcgong@gmail.com>
2015-07-01 19:13:41 +0800

    cleanup and rename the source files.

--- setup.py
+++ setup.py
@@ -2,12 +2,31 @@
 # -*- coding: utf-8 -*- 
 
 from distutils.core import setup
+from setuptools import find_packages
 
-setup(name='tornice',
-      version='0.1',
-      description='Elegant WebApp Server Framework in Tornado',
-      author='Chenggong Lyu',
-      author_email='lcgong@gmail.com',
-      url='https://github.com/lcgong/tornice',
-      packages=['tornice'],
-     )
+
+setup(
+    name='dpillars',
+    version='0.1',
+    license="BSD",
+    description='RESTful Framework with domain object',
+    author='Chenggong Lyu',
+    author_email='lcgong@gmail.com',
+    url='https://github.com/lcgong/dpillars',
+    packages=find_packages("src"),
+    package_dir = {"": "src"},
+    zip_safe = False,
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Utilities",
+        ],      
+    )
