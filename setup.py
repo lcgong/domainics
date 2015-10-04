65c3b89effa5433e014ef2af5b41a624a0a90588
Chenggong Lyu <lcgong@gmail.com>
2015-10-04 12:34:37 +0800

    prepare release v0.3.1

--- setup.py
+++ setup.py
@@ -4,10 +4,38 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+import os
+import sys
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    test_package_name = 'MyMainPackage'
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        _test_args = [
+            '--verbose',
+            '--ignore=build',
+            '--cov={0}'.format(self.test_package_name),
+            '--cov-report=term-missing',
+            # '--pep8',
+        ]
+        extra_args = os.environ.get('PYTEST_EXTRA_ARGS')
+        if extra_args is not None:
+            _test_args.extend(extra_args.split())
+        self.test_args = _test_args
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        from pkg_resources import normalize_path, _namespace_packages
+
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name='domainics',
-    version='0.3.0',
+    version='0.3.1',
     license="BSD",
     description='Web Framwork with Domain-orieted architecure',
     author='Chenggong Lyu',
@@ -32,4 +60,12 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Utilities",
         ],
+    test_suite='tests',
+    tests_require=[
+        'pytest',
+        'pytest-pep8',
+        'pytest-cov',
+        ],
+    cmdclass={'test': PyTest},
+
     )
