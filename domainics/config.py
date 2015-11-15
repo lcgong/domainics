d64ed7987224e9d1be20ee025dd7020928619482
Chenggong Lyu <lcgong@gmail.com>
2015-11-15 14:33:55 +0800

    refactor the home_path finder

--- domainics/config.py
+++ domainics/config.py
@@ -11,25 +11,31 @@ from domainics.db import set_dsn
 
 def _get_home_path():
 
-    main_file = sys.modules['__main__'].__file__
-    
-    homepath = fp.abspath(fp.dirname(main_file))
+    cur_path = fp.abspath(sys.modules['__main__'].__file__)
+    while True:
+        cur_path = fp.dirname(cur_path)
+        if fp.exists(fp.join(cur_path, '__init__.py')):
+            continue
+        break
 
-    
-    return homepath
+    if fp.exists(fp.join(cur_path, '__main__.py')):
+        return cur_path
+
+    raise ValueError('can not find home_path')
+
 
 def load(homepath=None):
 
     if homepath is None:
         homepath = _get_home_path()
-    
+
     print('homepath: ' + homepath)
 
     conf_dir = fp.join(homepath, 'conf')
-    
-    
+
+
     # 加载日志的配置文件
-    import logging.config    
+    import logging.config
     conf_file = fp.join(conf_dir, 'logging.yaml')
     if fp.exists(conf_file):
         with open(conf_file) as f:
@@ -39,12 +45,12 @@ def load(homepath=None):
     # logging.basicConfig()
 
     _logger = logging.getLogger(__name__)
-    _logger.info('configuration loaded, ' + conf_file) 
+    _logger.info('configuration loaded, ' + conf_file)
 
     abcs =logging.getLogger('abcs')
 
     abcs.info('test')
-     
+
     conf_file = fp.join(conf_dir, 'database.yaml')
     if fp.exists(conf_file):
         with open(conf_file) as f:
@@ -52,12 +58,12 @@ def load(homepath=None):
             for dsn, settings in conf.items():
                 if dsn.upper() == 'DEFAULT':
                     dsn = 'DEFAULT'
-                
-                try:                
+
+                try:
                     set_dsn(dsn=dsn, **settings)
                 except Exception as ex:
                     _logger.error(str(ex), exc_info=True)
 
-    _logger.info('configuration loaded, ' + conf_file)                       
+    _logger.info('configuration loaded, ' + conf_file)
 
 # load()
